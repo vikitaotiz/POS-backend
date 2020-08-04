dc6bc540c122dcc5e0236289b2be81c21968af27
vikitaotiz <victorotieno598@gmail.com>
2020-08-04 17:28:42 +0300

    New changes

--- app/Http/Resources/Users/UserResource.php
+++ app/Http/Resources/Users/UserResource.php
@@ -14,7 +14,7 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return return [
+        return [
             'id' => $this->id,
             'name' => $this->name,
             'department_id' => $this->department_id,
